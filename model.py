1e60d828a454e8df887efa25b96a4170c3748470
Cardoso <gabriel9225@gmail.com>
2022-05-25 23:09:23 -0300

    Limpeza de codigo

--- model.py
+++ model.py
@@ -20,6 +20,8 @@ GRAPH_PATH = RESOURCES + 'graphs/'
 DATA_PATH = RESOURCES + 'data/'
 TABLES_PATH = RESOURCES + 'tables/'
 LATEX_PATH = RESOURCES + 'latex/'
+BOX_PATH = GRAPH_PATH + 'box/'
+DIST_PATH = GRAPH_PATH + 'dist/'
 
 def determine_class_amount(data_set):
     return 1+ round(math.log(len(data_set), 2))
@@ -96,194 +98,18 @@ def coeff_variation(std_deviation, avg):
     if (avg == 0): return 0
     return std_deviation/avg * 100
 
-def latex_converter(entries):
-    tex = ""
-    i = 0
-    for entry in entries:
-        i += 1
-        tex += str(entry)
-        if not (i == len(entries)):
-            tex += ' & '
-        else:
-            tex += '\\' + '\\' + '\n'
-    return tex 
+def save_boxplot(df, x, file_name):
+    sns.set_theme(style="whitegrid")
 
-def save_latex_table(tex_table, file_name, subfolder):
-    text_file = open(LATEX_PATH + subfolder + file_name, "w")
-    n = text_file.write(tex_table)
-    text_file.close()
-
-def create_tables(data_set, name, file_name, descriptive):
-    empirical = latex_converter(["Classe", "Frequência", "F-Relativa", "%", "% Acumulada"])
-    with open(TABLES_PATH + file_name, 'w') as f:
-        with redirect_stdout(f):
-            print(name + '\n')
-            data_set.sort()
-            max_value = float(max(data_set))
-            min_value = float(min(data_set))
-            number_classes = determine_class_amount(data_set)
-            class_size = determine_class_size(data_set, number_classes, min_value, max_value)
-
-            entries_empirical = []
-            headers_empirical = [
-                'Classes',
-                'Frequência',
-                'F-Relativa',
-                '%',
-                '% Acumulada']
-            entries_descriptive = []
-            headers_descriptive = [
-                'Media',
-                'Q1',
-                'Mediana',
-                'Q3',
-                'L Inferior',
-                'L Superior',
-                'Moda',
-                "Variância",
-                "Desvio Padrão",
-                "Erro Padrão",
-                "Coeficiente de Variação",
-            ]
-
-            freq = []
-            accumulated_percentage = 0
-            for i in range(number_classes):
-                freq.append([])
-                inferior = min_value + i*class_size
-                if i == number_classes -1:
-                    superior = max_value + 0.001
-                else:
-                    superior = inferior + class_size
-                class_range = (str(format(inferior, ROUNDING_FORMAT)) + "|-" + str(format(superior, ROUNDING_FORMAT)))
-
-                for entry in data_set:
-                    if inferior <= float(entry) < superior:
-                        freq[i].append(float(entry))
-
-                percentage = len(freq[i])/len(data_set)
-                accumulated_percentage += percentage
-                entries_empirical.append([
-                    class_range, # Classes
-                    len(freq[i]), # Frequencia
-                    round(len(freq[i])/len(data_set), ROUNDING), # Frequência Relativa
-                    round(percentage, ROUNDING), # Porcentagem
-                    round(accumulated_percentage, ROUNDING) # Porcentagem acumulada
-                    ])
-                empirical += latex_converter([class_range, len(freq[i]), round(len(freq[i])/len(data_set), ROUNDING), round(percentage, ROUNDING), round(accumulated_percentage, ROUNDING)])
-                
-
-            avg = get_average(data_set)
-            variance = variance_sample(data_set)
-            std_deviation = math.sqrt(variance)
-            error_estimative = error_estimate(data_set, std_deviation)
-            q1 = get_Qx(data_set,1)
-            q3 = get_Qx(data_set,3)
-            alc_q1_q3 = (q3 - q1)
-            built_descriptive_entries = [
-                    round(avg, ROUNDING), # Media
-                    round(get_Qx(data_set,1), ROUNDING), #Q1
-                    round(q1, ROUNDING), # Mediana
-                    round(q3, ROUNDING), #Q3
-                    round(q1 - 1.5*alc_q1_q3, ROUNDING), #LimiteInferior
-                    round(q3 + 1.5*alc_q1_q3, ROUNDING), #LimiteSuperior
-                    most_frequent(data_set), # Moda
-                    round(variance, ROUNDING), # Variancia
-                    round(std_deviation, ROUNDING), # Desvio Padrão
-                    round(error_estimative, ROUNDING), # Erro Padrão
-                    round(coeff_variation(std_deviation, avg), ROUNDING), # Coeficiente de Variação
-            ]
-
-            built_descriptive = [
-                    file_name.replace(".txt", ""),
-                    round(avg, ROUNDING), # Media
-                    round(get_median(data_set), ROUNDING), # Mediana
-                    most_frequent(data_set), # Moda
-                    round(variance, ROUNDING), # Variancia
-                    round(std_deviation, ROUNDING), # Desvio Padrão
-                    round(error_estimative, ROUNDING), # Erro Padrão
-                    round(coeff_variation(std_deviation, avg), ROUNDING), # Coeficiente de Variação
-            ]
-
-            entries_descriptive.append(built_descriptive_entries)
-            descriptive += latex_converter(built_descriptive)
-
-            print(tabulate(entries_empirical, headers=headers_empirical, numalign="center", stralign="center"))
-            print()
-            print(str(round(class_size, ROUNDING)) + " tamanho das classes")
-            print(str(number_classes) + " numero de classes")    
-            print(str(len(data_set)) + " tamanho da amostra\n")
-            print(tabulate(entries_descriptive, headers=headers_descriptive, numalign="center", stralign="center"))
-
-            save_latex_table(empirical, file_name, "empirical/")
-
-            return descriptive
-
-
-
-def create_hist(data_set, color, label):
-    kwargs = dict(bins=determine_class_amount(data_set), color=color, label=label)
-
-    sns.distplot(data_set, **kwargs)
-
-def create_graph(label):
-    pyplot.grid(axis='y', alpha=0.75)
-    pyplot.xlabel('Valor (em R$)')
-    pyplot.ylabel('Frequencia')
-    pyplot.title(label)
-    pyplot.ylim(0,2)
-    pyplot.xlim(5,10)
-    pyplot.legend();
-    pyplot.xticks(np.arange(5, 10, 0.5))
-    manager = pyplot.get_current_fig_manager()
-    manager.window.showMaximized()
-    # fig = pyplot.gcf()
-    # fig.set_size_inches(18.5, 10.5)
-
-def get_price(set): 
-    return float(set['PRE_VENDA'].replace(",", "."))
-
-def save_histogram(dataset1, dataset2, color1, color2, label1, label2, title, file_name):
-    create_hist(dataset1, color1, label1) 
-    create_hist(dataset2, color2, label2) 
-
-    create_graph(title)
-    path = GRAPH_PATH + file_name
+    box_plot = sns.boxplot(data=df, x=x, y="PRE_VENDA", hue="ESTADO")
+    add_median_labels(box_plot, ROUNDING_FORMAT)
+   
+    path = BOX_PATH + file_name
     pyplot.savefig(path, )
-
     pyplot.figure().clear()
     pyplot.close()
     pyplot.cla()
     pyplot.clf()
-
-def save_boxplot(dataset1, dataset2, color1, color2, label1, label2, title, file_name):
-
-        data = {
-        label1: dataset1,
-        label2: dataset2
-        }
-        df = pds.concat([pds.DataFrame(v, columns=[k]) for k, v in data.items()], axis=1)
-
-
-        
-    
-
-        sns.set_theme(style="whitegrid")
-
-        box_plot = sns.boxplot(data=df
-
-        )
-        add_median_labels(box_plot, ROUNDING_FORMAT)
-        path = GRAPH_PATH + "box/" +file_name
-
-        pyplot.savefig(path, )
-
-        pyplot.figure().clear()
-        pyplot.close()
-        pyplot.cla()
-        pyplot.clf()
-
-
 
 def add_median_labels(ax, fmt='.1f'):
     lines = ax.get_lines()
@@ -300,72 +126,31 @@ def add_median_labels(ax, fmt='.1f'):
             path_effects.Stroke(linewidth=3, foreground=median.get_color()),
             path_effects.Normal(),
         ])
-def create_boxplot(data_set, color, label):
 
-    sns.boxplot(data_set)
+def save_dist(df, x, file_name):
+    # sns.set_theme(style="whitegrid")
+
+    sns.displot(df, x="PRE_VENDA", hue="ESTADO", col=x)
+   
+    path = DIST_PATH + file_name
+    pyplot.savefig(path, )
+    pyplot.figure().clear()
+    pyplot.close()
+    pyplot.cla()
+    pyplot.clf()
+
+def create_graphs(df, x, file_name):
+    save_boxplot(df, x, file_name)
+    save_dist(df, x, file_name)
 
 # Dados obtidos em 
 # https://preco.anp.gov.br/
 # Referentes aos dados de 01-05-22 a 07-05-22
-file = open(DATA_PATH + "anp-data.json")
-data = json.load(file)
+# file = open(DATA_PATH + "anp-data.json")
+# data = json.load(file)
 
-interior = []
-metropolis = []
+df = pds.read_json(DATA_PATH + "anp-data.json")
 
-nacional = []
-outras = []
-
-parana = []
-sp3 = []
-
-for set in data:
-
-    if set["REGIÃO"] == "Interior":
-        interior.append(get_price(set))
-    else: 
-        metropolis.append(get_price(set))
-
-    if set["BANDEIRA"] == "NACIONAIS":
-        nacional.append(get_price(set))
-    else:
-        outras.append(get_price(set))
-
-    if set["ESTADO"] == "PR":
-        parana.append(get_price(set))
-    else:
-        sp3.append(get_price(set))
-
-interior.sort()
-metropolis.sort()
-nacional.sort()
-outras.sort()
-parana.sort()
-sp3.sort()
-
-save_histogram(interior, metropolis, "darkgreen", "dodgerblue", "Interior", "Metrópolis", "SP3 (cidades de N a V) e Paraná - Interior x Metrópolis", "Interior-Metropolis")
-
-save_histogram(nacional, outras, "darkgreen", "orange", "Nacionais", "Outras", "SP3 (cidades de N a V) x Paraná - Bandeiras", "Bandeiras")
-
-save_histogram(parana, sp3, "limegreen", "red", "Paraná", "São Paulo", "SP3 (cidades de N a V) x Paraná", "SP3-Parana")
-
-descriptive = latex_converter(["Dados", "Média", "Mediana", "Moda", "Variância", "Desvio Padrão", "Erro Padrão", "Coef. Variação"])
-
-
-descriptive = create_tables(parana, "Paraná - Preço da GA", "parana.txt", descriptive)
-descriptive = create_tables(sp3, "SP3 (cidades de N a V) - Preço da GA", "sp3.txt", descriptive)
-
-descriptive = create_tables(nacional, "SP3 (cidades de N a V) e Paraná - Bandeiras Nacionais", "bandeiras-nacionais.txt", descriptive)
-descriptive = create_tables(outras, "SP3 (cidades de N a V) e Paraná - Outras Bandeiras", "outras-bandeiras.txt",descriptive)
-
-descriptive = create_tables(interior, "SP3 (cidades de N a V) e Paraná - Interior", "interior.txt", descriptive)
-descriptive = create_tables(metropolis, "SP3 (cidades de N a V) e Paraná - Metrópolis", "metropolis.txt", descriptive)
-# pyplot.show()
-descriptive = save_latex_table(descriptive, "descriptive", "descriptive/")
-
-save_boxplot(interior, metropolis, "darkgreen", "dodgerblue", "Interior", "Metrópolis", "SP3 (cidades de N a V) e Paraná - Interior x Metrópolis", "Interior-Metropolis")
-
-save_boxplot(nacional, outras, "darkgreen", "orange", "Nacionais", "Outras", "SP3 (cidades de N a V) x Paraná - Bandeiras", "Bandeiras")
-
-save_boxplot(parana, sp3, "limegreen", "red", "Paraná", "São Paulo", "SP3 (cidades de N a V) x Paraná", "SP3-Parana")
-
+create_graphs(df, "BANDEIRA", "Bandeira")
+create_graphs(df, "REGIÃO", "Região")
+create_graphs(df, "ESTADO", "Estados")
